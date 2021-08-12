c55b65505fec31946c129e812cafe8ca3c3e54ce
Miguel Sanchez <miguel.sanchex@gmail.com>
2021-08-12 00:53:23 -0500

    cors origin setup

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,10 @@ module AyendaTest
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => 'https://damp-fortress-91452.herokuapp.com',
+      'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
+    }
   end
 end
